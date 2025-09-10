3d0f4c31920bd43134ad5024813616930bc8cfd2
POTJANACHIT <POTJANA.CH.BUDC@cpird.in.th>
2025-09-10 16:33:48 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,9 +1,10 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyCpcsjSa52fI5hxvGzerXIc-Hz3U5vMOcryKSOMHqr_GJyWaEw-ST2VaSVOnG4YfD-/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbw8fFZb-WYaGNFod4V6UR6dtVEIRLm-_YKyi5irLwPqMgyMzJrm8F36GY77AQ9Ub4Un/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyWMQ2PmacSxINqdNNSgUGLnWeSxFJktT1C0chODg4GaMEbTNvpX8DQjz1jaMM2Gpo2/exec';
 //LIFF_ID
     const LIFF_ID = '2008088003-B4OLqrrK'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088003-pPxEgKKX'; // LIFF ID history
     const LIFF_REGISTER = '2008088003-ZKO6W77p'; // LIFF ID register
+
 
 
